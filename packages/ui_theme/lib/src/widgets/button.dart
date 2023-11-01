2f58004f42654360cd4140de7c4ffee725580bd3
Julian Sotelo <juansoos@protonmail.com>
2023-11-01 17:44:42 -0500

    Create onboarding UI

--- packages/ui_theme/lib/src/widgets/button.dart
+++ packages/ui_theme/lib/src/widgets/button.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ui_theme/src/widgets/platform/platform_widget.dart';
-import 'package:ui_theme/src/widgets/text.dart';
+import 'package:ui_theme/ui_theme.dart';
 
-class CustomButton extends PlatformWidget<CupertinoButton, ElevatedButton> {
-  const CustomButton({
+class CustomButtonFilled
+    extends PlatformWidget<CupertinoButton, ElevatedButton> {
+  const CustomButtonFilled({
     super.key,
     required this.text,
     this.onPressed,
@@ -26,6 +26,35 @@ class CustomButton extends PlatformWidget<CupertinoButton, ElevatedButton> {
     return CupertinoButton.filled(
       onPressed: onPressed,
       child: CustomText.title3(text: text),
+    );
+  }
+}
+
+class CustomButton extends PlatformWidget<CupertinoButton, TextButton> {
+  const CustomButton({super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final void Function()? onPressed;
+
+  @override
+  TextButton buildAndroidWidget(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: CustomText.title3(
+        text: text,
+        color: CustomColors.brown,
+      ),
+    );
+  }
+
+  @override
+  CupertinoButton buildIosWidget(BuildContext context) {
+    return CupertinoButton(
+      onPressed: onPressed,
+      child: CustomText.title3(
+        text: text,
+        color: CustomColors.brown,
+      ),
     );
   }
 }
